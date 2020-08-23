a3e1bde6e812699c137d615f59d3695a594eef6f
unknown <simoncsunas@gmail.com>
2020-08-23 16:39:54 +0900

    First Commit

---
+++ sSpeech/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace sSpeech
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fMain));
+            this.btnSpeech = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.rtbToSpeak = new System.Windows.Forms.RichTextBox();
+            this.txtRecognize = new System.Windows.Forms.TextBox();
+            this.btnRecognize = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSpeech
+            // 
+            this.btnSpeech.Location = new System.Drawing.Point(140, 205);
+            this.btnSpeech.Name = "btnSpeech";
+            this.btnSpeech.Size = new System.Drawing.Size(69, 24);
+            this.btnSpeech.TabIndex = 0;
+            this.btnSpeech.Text = "Speak";
+            this.btnSpeech.UseVisualStyleBackColor = true;
+            this.btnSpeech.Click += new System.EventHandler(this.btnSpeech_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(215, 205);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(69, 24);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "&Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // rtbToSpeak
+            // 
+            this.rtbToSpeak.Location = new System.Drawing.Point(13, 12);
+            this.rtbToSpeak.Name = "rtbToSpeak";
+            this.rtbToSpeak.Size = new System.Drawing.Size(280, 87);
+            this.rtbToSpeak.TabIndex = 2;
+            this.rtbToSpeak.Text = "";
+            // 
+            // txtRecognize
+            // 
+            this.txtRecognize.Location = new System.Drawing.Point(14, 106);
+            this.txtRecognize.Name = "txtRecognize";
+            this.txtRecognize.Size = new System.Drawing.Size(278, 20);
+            this.txtRecognize.TabIndex = 3;
+            // 
+            // btnRecognize
+            // 
+            this.btnRecognize.Location = new System.Drawing.Point(65, 205);
+            this.btnRecognize.Name = "btnRecognize";
+            this.btnRecognize.Size = new System.Drawing.Size(69, 24);
+            this.btnRecognize.TabIndex = 4;
+            this.btnRecognize.Text = "Recognize";
+            this.btnRecognize.UseVisualStyleBackColor = true;
+            this.btnRecognize.Click += new System.EventHandler(this.btnRecognize_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 241);
+            this.Controls.Add(this.btnRecognize);
+            this.Controls.Add(this.txtRecognize);
+            this.Controls.Add(this.rtbToSpeak);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSpeech);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.fMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpeech;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.RichTextBox rtbToSpeak;
+        private System.Windows.Forms.TextBox txtRecognize;
+        private System.Windows.Forms.Button btnRecognize;
+    }
+}
+
---
+++ sSpeech/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Speech.AudioFormat;
+
+
+namespace sSpeech
+{
+    public partial class fMain : Form
+    {
+
+        SpeechSynthesizer speaker = new SpeechSynthesizer();
+        SpeechRecognitionEngine speechrecognize = new SpeechRecognitionEngine();
+      
+        PromptBuilder pb = new PromptBuilder();
+
+        Choices clist;
+
+        public fMain()
+        {
+            InitializeComponent();
+
+            speechrecognize.SpeechRecognized += speechrecognize_SpeechRecognized;
+        }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+            Grammar lsGrammar = new Grammar(new GrammarBuilder("hello"));
+
+            try
+            {
+                speechrecognize.RequestRecognizerUpdate();
+                speechrecognize.LoadGrammar(lsGrammar);
+                speechrecognize.SetInputToDefaultAudioDevice();
+                speechrecognize.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Recognize", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            speechrecognize.RecognizeAsyncStop();
+            this.Close();
+        }
+
+        private void btnSpeech_Click(object sender, EventArgs e)
+        {
+            speaker.Rate = 1;
+            speaker.Volume = 100;
+            speaker.SelectVoice("Microsoft Anna");
+            speaker.Speak(rtbToSpeak.Text);
+        }
+
+        private void btnRecognize_Click(object sender, EventArgs e)
+        {
+            //clist.Add(new string[] { "hello", "how are you", "thank you", "close" });
+            Grammar lsGrammar = new Grammar(new GrammarBuilder(txtRecognize.Text.Trim()));
+           
+            try
+            {
+                speechrecognize.RequestRecognizerUpdate();
+                speechrecognize.LoadGrammar(lsGrammar);            
+                speechrecognize.SetInputToDefaultAudioDevice();
+                speechrecognize.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Recognize", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        void speechrecognize_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text.ToString())
+            {
+                case "hello":
+                    speaker.Speak("Hello Simon");
+                    break;
+                case "how are you":
+                    speaker.SpeakAsync("fine thank you");
+                    break;
+                case "close":
+                    speaker.SpeakAsync("the system will closed");
+                    Application.Exit();
+                    break;
+            }
+        }
+    }
+}
